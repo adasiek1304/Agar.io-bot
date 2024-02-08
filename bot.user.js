40787b53c8891237b60da1c10d588cbd2788bd7d
adasiek1304 <159468358+adasiek1304@users.noreply.github.com>
2024-02-08 18:35:07 +0100

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -21,8 +21,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
 // ==UserScript==
-// @name        AposBot
-// @namespace   AposBot
+// @name        Adamgamesbot.pl
+// @namespace   Adamgamesbot.pl
 // @include     http://agar.io/*
 // @version     3.651
 // @grant       none
